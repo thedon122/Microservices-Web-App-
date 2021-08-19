40e652800cb8ff767591205546e8f11c80d2fe5f
thedon122 <donovan122@yahoo.com>
2021-08-18 23:37:21 -0400

    updates

---
+++ admin/products/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, status
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+    
+    def create(self, request):
+    
+    def retrieve(self, request, pk=None):
+    
+    def update(self, request, pk=None):
+
+    def destroy(self, request, pk=None):
