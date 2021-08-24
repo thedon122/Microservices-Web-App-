5aee9bcc4180c82e32e612e22196d3754d31cb1c
thedon122 <donovan122@yahoo.com>
2021-08-23 23:51:56 -0400

    update

---
+++ main/main.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from flask import Flask, jsonify, abort
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+@dataclass
+class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
