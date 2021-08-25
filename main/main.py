bd8147949d76ae2e0f070eee53c74d889bf643b3
thedon122 <donovan122@yahoo.com>
2021-08-24 23:54:05 -0400

    update main

--- main/main.py
+++ main/main.py
@@ -18,3 +18,16 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
+
+@dataclass
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+
+
+@app.route('/api/products')
+def index():
+    return jsonify(Product.query.all())
