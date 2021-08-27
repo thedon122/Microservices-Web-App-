3b3a44ef966d95bc6a32428dda988f0f86057251
thedon122 <donovan122@yahoo.com>
2021-08-26 23:11:00 -0400

    main update

--- main/main.py
+++ main/main.py
@@ -31,3 +31,26 @@ class ProductUser(db.Model):
 @app.route('/api/products')
 def index():
     return jsonify(Product.query.all())
+
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://docker.for.mac.localhost:8000/api/user')
+    json = req.json()
+
+    try:
+        productUser = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+
+        publish('product_liked', id)
+    except:
+        abort(400, 'You already liked this product')
+
+    return jsonify({
+        'message': 'success'
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
