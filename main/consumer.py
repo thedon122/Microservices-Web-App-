d37b84260ac4926515bb34197270160794d85c64
thedon122 <donovan122@yahoo.com>
2021-08-31 00:08:52 -0400

    update main

---
+++ main/consumer.py
@@ -0,0 +1,23 @@
+import pika, json
+
+from main import Product, db
+
+params = pika.URLParameters('your_rabbitmq_url')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='main')
+
+
+def callback(ch, method, properties, body):
+    print('Received in main')
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
