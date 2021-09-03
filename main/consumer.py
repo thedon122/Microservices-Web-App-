3246a26ef091dae369663c4ac0c1b91b0e883ea7
thedon122 <donovan122@yahoo.com>
2021-09-02 23:56:53 -0400

    update main

--- main/consumer.py
+++ main/consumer.py
@@ -21,3 +21,24 @@ def callback(ch, method, properties, body):
         db.session.add(product)
         db.session.commit()
         print('Product Created')
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
+
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
+
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
